fdbd45460392c0d23b11ae4b20e4c6efb3c0bf4e
sheena849 <sheenamugo04@gmail.com>
2024-11-04 12:10:14 +0300

    Event Handling in React Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe(){
+  function handleFocus(e){
+    console.log('Good!')
+  }
+  function handleBlur(e){
+    console.log('Hey! Eyes on me!')
+  }
+  return(
+    <div>
+      <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+export default EyesOnMe;
